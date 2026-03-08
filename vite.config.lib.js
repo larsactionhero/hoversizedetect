db98fc88bea719a9e17e65d0cf5c9d3f201695c9
Lars Ortlepp <job@larsactionhero.com>
2026-03-08 11:06:59 +0100

    enable minify for porduction build

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -17,6 +17,7 @@ export default defineConfig({
   build: {
     outDir: resolve(path, './dist'),
     emptyOutDir: false,
+    minify: 'esbuild',
     lib: {
       entry: resolve(path, './index.js'),
       name: 'HoverSizeDetect',
